4eff7c6a64c4545f61fe8ec909f7c6e28131ce5f
Phil Smith <philsmith@Phils-MacBook-Pro.local>
2017-11-27 13:18:53 +0000

    Initial Commit

---
+++ Cloud_Skeletonization/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cloud_Skeletonization
+//
+//  Created by Phil Smith on 27/11/2017.
+//  Copyright © 2017 Phil Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
